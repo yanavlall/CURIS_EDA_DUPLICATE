ab2416d04c316a969451982b97115033d5d9aa5b
yanavlall <yanav@stanford.edu>
2024-06-25 11:58:16 -0700

    Initialization

---
+++ CURIS_EDA_v1/CURIS_EDA_v1/CURIS_EDA_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  CURIS_EDA_v1App.swift
+//  CURIS_EDA_v1
+//
+//  Created by Yanav Lall on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CURIS_EDA_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CURIS_EDA_v1/CURIS_EDA_v1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CURIS_EDA_v1
+//
+//  Created by Yanav Lall on 25/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
