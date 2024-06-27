05a91de0ae6be4892c87c72b74a7ecc3f4d7d0cf
Katie Liu <122421205+katieliuu@users.noreply.github.com>
2024-06-27 12:15:15 -0700

    Add files via upload

---
+++ simple-watch-connectivity-swift/MyAppleWatchCompanionApp Watch App/MyAppleWatchCompanionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppleWatchCompanionAppApp.swift
+//  MyAppleWatchCompanionApp Watch App
+//
+//  Created by Giovanni Tjahyamulia on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MyAppleWatchCompanionApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ simple-watch-connectivity-swift/MyAppleWatchCompanionApp Watch App/WatchSession.swift
@@ -0,0 +1,61 @@
+//
+//  WatchSession.swift
+//  MyAppleWatchCompanionApp Watch App
+//
+//  Created by Giovanni Tjahyamulia on 20/10/23.
+//
+
+// 1. import
+import Foundation
+import WatchConnectivity
+import WatchKit
+
+class WatchSession: NSObject, ObservableObject {
+    // 2. initialize WCSession
+    var wcSession: WCSession?
+    
+    // 7. initialize published variable to get data
+    @Published var receivedData: String = "Haven't received any data"
+    
+    // 3. init
+    override init() {
+        super.init()
+        
+        // 4. WCSession delegate and activate
+        if WCSession.isSupported() {
+            wcSession = WCSession.default
+            wcSession?.delegate = self
+            wcSession?.activate()
+        }
+    }
+}
+
+// 5. delegate WCSession
+extension WatchSession: WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+        // do something when active
+        if let error = error {
+            print("WCSession activation failed with error: \(error.localizedDescription)")
+            return
+        }
+        print("WCSession activated with state: \(activationState.rawValue)")
+    }
+    
+    // 6. receive data
+    // a. via Message
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        if let data = message["key"] as? String {
+            DispatchQueue.main.async {
+                self.receivedData = data
+                
+                // trigger haptic feedback on the watch
+                WKInterfaceDevice.current().play(.success)
+            }
+        }
+    }
+    
+    func sessionReachabilityDidChange(_ session: WCSession) {
+        // handle reachability changes if necessary
+    }
+}
