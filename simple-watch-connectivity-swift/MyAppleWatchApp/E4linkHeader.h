6b3d8d95b897d51f478e3c7251ea76672572c999
Katie Liu <122421205+katieliuu@users.noreply.github.com>
2024-07-08 16:31:09 -0700

    Move E4link

--- simple-watch-connectivity-swift/MyAppleWatchApp/E4linkHeader.h
+++ simple-watch-connectivity-swift/MyAppleWatchApp/E4linkHeader.h
@@ -8,6 +8,6 @@
 #ifndef E4linkHeader_h
 #define E4linkHeader_h
 
-#import "E4link.framework/Versions/Current/Headers/E4link.h"
+#import <../E4link.framework/Versions/Current/Headers/E4link.h>
 
 #endif /* E4linkHeader_h */
