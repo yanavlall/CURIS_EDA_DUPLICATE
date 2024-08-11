e5d89184c969df292fd344b93cd89daf0589c4f8
Katie Liu <122421205+katieliuu@users.noreply.github.com>
2024-08-11 12:54:20 -0400

    Small change - no reset workout

--- simple-watch-connectivity-swift/MyAppleWatchCompanionApp/AppDelegate.swift
+++ simple-watch-connectivity-swift/MyAppleWatchCompanionApp/AppDelegate.swift
@@ -12,7 +12,6 @@ class AppDelegate: NSObject, WKApplicationDelegate {
     func handle(_ workoutConfiguration: HKWorkoutConfiguration) {
         Task {
             do {
-                workoutManager.resetWorkout()
                 try await workoutManager.startWorkout(workoutConfiguration: workoutConfiguration)
                 print("Successfully started workout.")
             } catch {
